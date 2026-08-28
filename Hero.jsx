0998570db46d2ca9f5b998dfeeb176f6fd279cbf
Shikhakumari567567 <shikhakumari25@navgurukul.org>
2026-08-28 10:48:29 +0530

    Add files via upload

---
+++ Hero.jsx
@@ -0,0 +1,26 @@
+function Hero() {
+  return (
+    <section className="hero" id="home">
+      <div className="hero-content">
+        <p className="small-title">🌸 A Celebration of Love 🌸</p>
+
+        <h1>Happy Raksha Bandhan</h1>
+
+        <p>
+          Celebrating the beautiful bond of love, care and togetherness
+          between brothers and sisters.
+        </p>
+
+        <a href="#celebrate" className="hero-btn">
+          Celebrate Now 🎉
+        </a>
+      </div>
+
+      <div className="rakhi">
+        🪷
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
