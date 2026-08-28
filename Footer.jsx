e06716f471234149b309dc9b53dc2b3babcc4d2a
Shikhakumari567567 <shikhakumari25@navgurukul.org>
2026-08-28 10:48:08 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <h3>🪷 Rakhi Celebration</h3>
+      <p>Made with ❤️ for the beautiful bond of siblings.</p>
+    </footer>
+  );
+}
+
+export default Footer;
