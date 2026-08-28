343fe0b06ea0fba0a4423f286ec567aa81b5a619
Shikhakumari567567 <shikhakumari25@navgurukul.org>
2026-08-28 10:48:49 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>🪷 Rakhi Celebration</h2>
+
+      <div className="nav-links">
+        <a href="#home">Home</a>
+        <a href="#celebrate">Celebrate</a>
+        <a href="#wishes">Wishes</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
